3e10ae0ac64c61cf52bb9faa41c8b42454bba3d2
mmbeeb <88510227+mmbeeb@users.noreply.github.com>
2021-09-17 13:13:13 +0100

    Update main.c
    Some changes to the timing so it works in Cygwin64, specifically using clock_gettime() rather than clock().  I found the latter was only incrementing 64 times per second, so the emulator ran very slow.

--- main.c
+++ main.c
@@ -2,6 +2,8 @@
 /* main.c                 */
 /* (c) 2021 Martin Mather */
 
+/* 17/09/21, some changes made to make it run in Cygwin64 */
+
 #define _POSIX_C_SOURCE 1
 
 #include <ctype.h>
@@ -12,12 +14,15 @@
 #include <sys/ioctl.h>
 #include <unistd.h>	//sleep()
 #include <time.h>
+#include <sys/socket.h> //for Cygwin
 
 #include "aun.h"
 #include "fsem.h"
 
-#define LOOPSPERSEC	3000
-#define OPSPERLOOP	1000
+//#define LOOPSPERSEC	3000
+//#define OPSPERLOOP	1000
+
+#define OPSPERSEC	2000000
 
 void set_no_buffer() {
 	struct termios term;
@@ -38,8 +43,14 @@ int charsWaiting(int fd) {
 int main(void) {
 	char c;
 	int ex = 0, loops = 0;
-	double t2 = (double) 1/1000;
-	clock_t timeout1 = time(0), timeout2 = clock();
+	//double t2 = (double) 1/1000;
+	clock_t timeout1 = time(0); //, timeout2 = clock();
+	
+	//21/09/21 new timing using clock_gettime
+	double lps = 1000;
+	int ops = OPSPERSEC/lps;
+	long ntime = 0, otime = 0;
+	struct timespec ts;
 
 	printf("File Server Emulator\n\n");
 
@@ -50,19 +61,31 @@ int main(void) {
 		do {
 			if (time(0) > timeout1 && loops) {//second timer
 				//printf("loop timeout %d %f\n", loops, t2);
-
-				t2 = t2 *((double) loops/LOOPSPERSEC);
+				//t2 = t2 *((double) loops/LOOPSPERSEC);
+				
+				lps = (lps + loops)/2;//Average
+				ops = OPSPERSEC/lps;
+				//printf(":: loops=%d lps=%f ops=%d\n", loops, lps, ops);
+				
 				loops = 0;
 				timeout1 = time(0);
 			}
 
-			if (clock() > timeout2) {
+			//if (clock() > timeout2) {
+			clock_gettime(CLOCK_MONOTONIC, &ts);
+			ntime = ts.tv_nsec/1000000;//milliseconds
+			if (ntime != otime) {
+				otime = ntime;
+				
 				loops++;
 				aun_poll();
 
+				//if (!ex)
+				//	ex = fsem_exec(OPSPERLOOP, 0);
+				
 				if (!ex)
-					ex = fsem_exec(OPSPERLOOP, 0);
-	
+					ex = fsem_exec(ops, 0);
+				
 				c = 0;
 				int count = charsWaiting(fileno(stdin));
 				if (count != 0) {
@@ -88,7 +111,7 @@ int main(void) {
 					}
 				}
 
-				timeout2 = clock() + t2 * CLOCKS_PER_SEC;
+				//timeout2 = clock() + t2 * CLOCKS_PER_SEC;
 			}
 		} while (c != 'q');
 	
